f157543e82190209274d2a78143cfa58de3af7b7
aidanbell0 <aidanbell0182@gmail.com>
2020-01-21 12:23:46 -0500

    Starting weather app in React sprint

---
+++ src/Components/Weather/Weather.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Weather extends Component {
+  render() {
+    return(
+      <div>
+        <h2>weather bois</h2>
+      </div>
+    )
+  }
+}
+
+export default Weather;
