a0279bc77c087755b5bcf7d8c378bf5b9583f24f
aidanbell0 <aidanbell0182@gmail.com>
2020-01-22 14:03:24 -0500

    Forecast is being separated by date. Next is to render cards

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Loading from '../Loading/Loading';
+import ForecastCard from '../ForecastCard/ForecastCard';
 
 import 'weather-icons/css/weather-icons.css';
 import './Forecast.css';
@@ -7,7 +8,54 @@ import './Forecast.css';
 class Forecast extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      indivHrs: null,
+      days: null
+    }
+  }
 
+  // Write:
+  // Array/Object destructuring
+  // Date Parsing formula
+  //
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate() {
+    if (!this.state.indivHrs) {
+      let indivHrs = [];
+      this.props.forecast.list.map(f => {
+        indivHrs.push(f);
+      });
+      this.setState({
+        indivHrs: indivHrs
+      });
+      console.log('just set hours');
+    }
+    console.log(this.state.indivHrs)
+    if (!this.state.days && this.state.indivHrs) {
+      this.splitDays(this.state.indivHrs);
+    }
+  }
+
+  splitDays = (hrs) => {
+    let forecast = [];
+    let day = [];
+    let prev = null;
+    hrs.map(i => {
+      if (prev === null) { prev = i.dt_txt.slice(8,10) }
+      if (i.dt_txt.slice(8,10) === prev) {
+        day.push(i);
+      } else {
+        forecast.push(day);
+        day = [];
+        prev = i.dt_txt.slice(8,10);
+      }
+    })
+    this.setState({
+      days: forecast
+    })
   }
 
   render() {
