536c5ae94daab2af6c6bbb1c01da46ec1f0e4fdf
Aidan Bell <aidanbell0182@gmail.com>
2021-07-28 17:06:34 -0400

    update key

--- src/services/weather-api.js
+++ src/services/weather-api.js
@@ -1,12 +1,12 @@
 export function getCurWeather(lat, lng, units) {
-  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=${units}&appid=a1c7cdd90159b4459ed96cc67ae2b3ca`;
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=${units}&appid=f610830a383051d4b21cd59f0ad5f08a`;
   return fetch(endpoint, {
     mode: 'cors'
   }).then(res => res.json());
 }
 
 export function getForecast(lat, lng, units) {
-  const endpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=${units}&appid=a1c7cdd90159b4459ed96cc67ae2b3ca`;
+  const endpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=${units}&appid=f610830a383051d4b21cd59f0ad5f08a`;
   return fetch(endpoint, {
     mode: 'cors'
   }).then(res => res.json());
